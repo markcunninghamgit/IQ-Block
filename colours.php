f793a2289afea7a9d75f3af35e42655f6bb32b5f
Mark Cunningham <contact@markcunningham.ie>
2012-12-30 02:18:52 +0000

    colours.php file. Contains arrays of the different shapes by colour represented in 2d array

---
+++ colours.php
@@ -0,0 +1,113 @@
+<?php
+
+	//Default blank colour unused by anything. Left here for copy paste purposes to add shapes. 
+	//4x4 was the most common but there are shapes with less and some with more
+	$colour = array(
+		array(0,0,0,0),
+		array(0,0,0,0),
+		array(0,0,0,0),
+		array(0,0,0,0)
+		);
+
+
+
+	$blue = array(
+		array(1,1,0),
+		array(1,1,1),
+		array(1,1,1)
+		);
+
+
+
+	$yellow = array(
+		array(1,1,0,0),
+		array(1,1,0,0),
+		array(1,1,0,0),
+		array(1,1,1,1)
+		);
+
+
+	$lightPurple = array(
+		array(1,0,0,1),
+		array(1,0,0,1),
+		array(1,0,0,1),
+		array(1,1,1,1)
+		);
+
+
+
+	$darkPurple = array(
+		array(0,1,1,0),
+		array(0,1,1,0),
+		array(0,1,1,0),
+		array(1,1,1,1)
+		);
+
+
+
+	$lightGreen = array(
+		array(1,1,0,0),
+		array(1,1,0,0),
+		array(0,0,1,1),
+		array(0,0,1,1)
+		);
+
+
+
+
+	$darkGreen = array(
+		array(0,1,0),
+		array(0,1,0),
+		array(1,1,1),
+		array(1,1,1)
+		);
+
+
+
+
+	$orange = array(
+		array(1,1,1,1),
+		array(1,1,1,1)
+		);
+
+
+
+
+	$pink = array(
+		array(0,0,1,1,1),
+		array(1,1,1,1,1)
+		);
+
+
+
+
+//Use these colours for verifying rotate functions work
+	$testColour1= array(
+		array(1,2,3,4,5),
+		array(6,7,8,9,10)
+		);
+
+	$testColour1R = array(
+		array(6,1),
+		array(7,2),
+		array(8,3),
+		array(9,4),
+		array(10,5)
+		);
+
+
+
+	$testColour2 = array(
+		array(1,2,3),
+		array(4,5,6),
+		array(7,8,9)
+		);
+
+	$testColour2R = array(
+		array(7,4,1),
+		array(8,5,2),
+		array(9,6,3)
+		);
+
+
+
