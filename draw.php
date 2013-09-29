7ac7f8bfbf302b1434511e31c566c8b70f780bf3
Mark Cunningham <contact@markcunningham.ie>
2013-09-29 09:13:27 +0100

    removed duplicate function findlefttopsquare. exists in functions.php

--- draw.php
+++ draw.php
@@ -46,38 +46,5 @@ $colour = rotateColour($colourDB["yellow"],3);
 $myBlock = addShape($myBlock,$colour);
 printBlock($myBlock);
 
-/*
-	Searches for the (left most) then (top most) square. 
-	Used for: shapes + blocks
-	returns array of 2 values: i,j coordinates of shape array where square is
-*/
-function findLeftTopSquare($shape, $taken = true)
-{
-	$rowCount = count($shape);
-	$columnCount = count($shape[0]);
-
-	//Start searching down the columns then extend the row as we go
-	for ($i=0; $i<$columnCount; $i++)
-	{
-		for ($j=0; $j < $rowCount; $j++)
-		{
-			//Find the first instance of findme (default 1)
-			if ($taken == true and $shape[$j][$i] != 0)
-			{
-				return array($j,$i);
-			}
-
-			if ($taken == false and $shape[$j][$i] == 0)
-			{
-				return array($j,$i);
-			}
-
-
-		}
-	}
-	throw new Exception('when searching for the top most left sqaure occupied by a shape or block, we found none... have we got a blank shape? if not something went wrong');
-
-}
-
 
 
